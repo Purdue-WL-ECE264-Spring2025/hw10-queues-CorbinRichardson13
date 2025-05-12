1d5b1e4de7bd6d9d0672b77ba127ee54558fad4f
Corbin Thomas Richardson <richa578@eceprog1.ecn.purdue.edu>
2025-05-12 00:59:02 -0400

    Added some functionality

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,58 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+  struct list_node * new = malloc(sizeof(struct list_node));
+  new->value = value; 
+  return new; 
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node * new = new_node(value);
+  new->next = list->head;
+  list->head = new;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node * new = new_node(value);
+  new->next = NULL;
+  if(list->head == NULL) {
+    list->head = new;
+    return;
+  }
+  struct list_node * cur = list->head;
+  while(cur->next != NULL) {
+    cur = cur->next;
+  }
+  cur->next = new;
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  size_t cur_value = list->head->value;
+  list->head = list->head->next;
+  return cur_value; 
+}
 
-void free_list(struct linked_list list) {}
+size_t remove_from_tail(struct linked_list *list) { 
+  struct list_node * cur = list->head;
+  while(cur->next != NULL) {
+    cur = cur->next;
+  }
+  size_t cur_value = cur_value;
+  cur->next = NULL;
+  return cur_value; 
+}
+
+void free_list(struct linked_list list) {
+  struct list_node * cur = list.head;
+  struct list_node * next;
+  while (cur != NULL) {
+    next = cur->next;
+    free(cur);
+    cur = next;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,18 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    uint64_t cur_state = serialize(state);
+    insert_at_head(&q->data, cur_state);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+    size_t cur_value = remove_from_tail(&q->data);
+    struct game_state cur_game_state = deserialize(cur_value);
+    return (cur_game_state); 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) { 
+
+    return 0; 
+}
